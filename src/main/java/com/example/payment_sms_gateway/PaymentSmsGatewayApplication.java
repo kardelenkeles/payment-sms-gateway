a9213176c5229088dca4afa853b6e197bf3cfa29
kardelenkeles <kar_di44@hotmail.com>
2025-06-14 18:37:26 +0300

    rename DemoApplication to PaymentSmsGatewayApplication and update project description

--- src/main/java/com/example/payment_sms_gateway/PaymentSmsGatewayApplication.java
+++ src/main/java/com/example/payment_sms_gateway/PaymentSmsGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PaymentSmsGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PaymentSmsGatewayApplication.class, args);
 	}
 
 }
